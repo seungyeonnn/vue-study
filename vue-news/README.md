f85467b4888a307825f782714b5e969825fd0b60
seungyeonnn <tiger_0724@naver.com>
2024-01-10 10:10:19 +0900

    feat: Complete section 10

---
+++ vue-news/README.md
@@ -0,0 +1,40 @@
+# vue-news
+
+<br />
+
+## typeScript 점진적으로 적용하기
+
+### error) 1. main.ts의 .js 파일 error
+
+![Alt text](image.png)
+error: type에 any가 붙어 있어야 하는데 any가 없어!
+-> strict 옵션이 true여서
+<br />
+
+- strict 옵션 level 낮추기
+  tsconfig.json
+  ![Alt text](image-1.png)
+  "strict"를 false로 "noImplicitAny": false를 추가해주면 에러 해결 <br />
+  noImplicitAny -> 암묵적으로 any 타입을 가지는 것을 허락 x
+  <br /><br />
+
+### error) 2. main.ts의 router error
+
+![Alt text](image-2.png)
+
+- allowJS 추가
+  tsconfig.json
+  ```
+  "allowJS" : true
+  ```
+
+<br />
+
+---
+
+<br />
+
+## .js -> .ts로 바꾸기
+
+1. 기본적인 타입 에러 해결하기
+2. 구체적으로 타입을 정해줄 수 있는 곳에 타입 정의
---
+++ vue-news/src/routes/index.ts
@@ -0,0 +1,88 @@
+import Vue from "vue";
+import VueRouter, { Route, NavigationGuardNext } from "vue-router";
+import { ItemView, UserView } from "../views";
+import createListView from "../views/CreateListView";
+import bus from "../utils/bus";
+import store from "../store/index.js";
+
+Vue.use(VueRouter);
+
+export default new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      redirect: "/news",
+    },
+    {
+      path: "/news",
+      name: "news",
+      component: createListView("NewsView"),
+      // 세번째 인자가 호출되면 다음으로 넘어감
+      async beforeEnter(
+        routeTo: Route,
+        routeFrom: Route,
+        next: NavigationGuardNext<Vue>
+      ) {
+        bus.$emit("on:progress");
+        try {
+          await store.dispatch("FETCH_LIST", routeTo.name);
+          next();
+        } catch (error) {
+          console.log("failed to fetch news items");
+        }
+        // .dispatch("FETCH_LIST", routeTo.name)
+        // .then(() => next())
+        // .catch(() => new Error("failed to fetch news items"));
+      },
+    },
+    {
+      path: "/ask",
+      name: "ask",
+      component: createListView("AskView"),
+      beforeEnter(routeTo, routeFrom, next) {
+        bus.$emit("on:progress");
+        store
+          .dispatch("FETCH_LIST", routeTo.name)
+          .then(() => next())
+          .catch(() => new Error("failed to fetch news items"));
+      },
+    },
+    {
+      path: "/jobs",
+      name: "jobs",
+      component: createListView("JobsView"),
+      beforeEnter(routeTo, routeFrom, next) {
+        bus.$emit("on:progress");
+        store
+          .dispatch("FETCH_LIST", routeTo.name)
+          .then(() => next())
+          .catch(() => new Error("failed to fetch news items"));
+      },
+    },
+    {
+      path: "/item/:id",
+      component: ItemView,
+      beforeEnter(routeTo, routeFrom, next) {
+        bus.$emit("on:progress");
+        const itemId = routeTo.params.id;
+        store
+          .dispatch("FETCH_ITEM", itemId)
+          .then(() => next())
+          .catch((err) => new Error("failed to fetch item details"));
+      },
+    },
+    {
+      path: "/user/:id",
+      component: UserView,
+      beforeEnter(routeTo, routeFrom, next) {
+        bus.$emit("on:progress");
+        const itemId = routeTo.params.id;
+        store
+          .dispatch("FETCH_USER", itemId)
+          .then(() => next())
+          .catch((err) => new Error("failed to fetch user profile"));
+      },
+    },
+  ],
+});
